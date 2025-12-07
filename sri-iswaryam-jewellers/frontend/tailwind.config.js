8a563c688ba0c85e01de15a81830632e0e391211
Arjun-Sankar <arjunsankarchitra@gmail.com>
2025-12-07 05:46:18 +0530

    Brown with gold

--- sri-iswaryam-jewellers/frontend/tailwind.config.js
+++ sri-iswaryam-jewellers/frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#4A2C2A',
+          dark: '#2D1B1A',
+          light: '#6B4545',
+        },
+        brand: {
+          brown: '#4A2C2A',
+          'brown-dark': '#2D1B1A',
+          'brown-light': '#6B4545',
+          gold: '#D4AF37',
+          'gold-dark': '#B8960C',
+          'gold-light': '#F4D03F',
+        },
+        accent: {
+          gold: '#D4AF37',
+        }
+      },
+    },
   },
   plugins: [],
 }
